a4b92b130ec5deda5489846f589c6a8c5965e9b3
Jim Thorne <jamesthorne@Jamess-MacBook-Air.local>
2020-01-13 20:56:45 -0700

    first commit

---
+++ number18.js
@@ -0,0 +1,39 @@
+let selectedThrill = "Jeeping";
+
+// Pertains to Jeeping
+
+if (selectedThrill === "Jeeping") {
+    // If thrill is Jeeping 
+    // Program executes per program designers choice
+    console.log ("You selected Jeeping, what a thrill");
+} else {
+    // If not Jeeping do this
+    // Program executes per designers choice
+    console.log ("You did not select Jeeping")    
+}
+
+// Pertains to skiing
+if (selectedThrill === "skiing") {
+    // If thrill is skiing 
+    // Program executes per program designers choice
+    console.log ("You selected skiing, what a thrill");
+} else {
+    // If not skiing do this
+    // Program executes per designers choice
+    console.log ("You did not select skiing")    
+}
+/*
+Expected Result:
+You selected Jeeping, what a thrill
+AND
+You did not select skiing
+
+HERES WHY
+Because the if else statement about Jeeping met the condition the
+console log about selecting Jeeping occurred. 
+Because the selected thrill was not skiing the statement 'You did not 
+select skiing was executed*/
+
+
+
+
